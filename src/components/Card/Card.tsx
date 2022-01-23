0d2480d065e16f587a7e56336fbceb9b46804e70
Temirlan <amadeusarkhammorningstar@gmail.com>
2022-01-24 03:36:09 +0600

    fix(Card): refactor styles to styled components

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import '../../styles/card.scss';
+import styled from 'styled-components';
+
+
+const CardItem = styled.div`
+    background-color: #fff;
+    min-width: 220px;
+    min-height: 50px;
+    box-shadow: 0 1px 0 #091e4240;
+    cursor: pointer;
+    padding: 3px 0px 0px 10px;
+    margin-bottom: 10px;`
+
+
 interface CardProps {
     title : string,
     key : number
 }
 const Card = ({title} : CardProps) => {
     return (
-        <div className='card'>
+        <CardItem className='card'>
             <samp>{title}</samp>
-        </div>
+        </CardItem>
     )
 }
 
