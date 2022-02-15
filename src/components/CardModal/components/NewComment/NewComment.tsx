6a4673285157b7a5d54fc28b35d02738edccf71c
Temirlan <amadeusarkhammorningstar@gmail.com>
2022-02-16 00:48:00 +0600

    update(NewComment): delete unused variables and functions

--- src/components/CardModal/components/NewComment/NewComment.tsx
+++ src/components/CardModal/components/NewComment/NewComment.tsx
@@ -1,12 +1,11 @@
 import React, { FC, useRef, useState } from "react";
 import styled from "styled-components";
-import { COLORS } from "styles/colors";
 import { Button } from "components/ui/components/Button";
-import { Textarea } from "components/ui/components/Textarea";
 import { useDispatch, useSelector } from "react-redux";
 import { addComment } from "redux/ducks/Comments/CommentsSlice";
 import { selectUser } from "redux/selectors";
 import { Form, Field } from "react-final-form";
+import { TextInput } from "components/ui/components/TextInput";
 
 interface NewCommentProps {
   cardId: string;
@@ -16,37 +15,24 @@ type Value = {
   newComment: string;
 };
 const NewComment: FC<NewCommentProps> = ({ cardId }) => {
-  const ref = useRef<HTMLTextAreaElement>(null);
   const authorData = useSelector(selectUser);
   const author = authorData.name;
   const dispatch = useDispatch();
-  const [newComment, setNewComment] = useState("");
 
   const onSubmit = (value: Value) => {
     const comment = value.newComment.trim();
     if (comment) {
       dispatch(addComment({ cardId, comment, author }));
-      setNewComment("");
-      ref?.current?.blur?.();
     }
   };
 
-  const handleOnKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key === "Enter") {
-      const comment = newComment.trim();
-      if (comment) {
-        dispatch(addComment({ cardId, comment, author }));
-        setNewComment("");
-        ref?.current?.blur?.();
-      }
-    }
-  };
+
 
   return (
     <NewCommentContainer>
       <Form
         onSubmit={onSubmit}
-        render={({ handleSubmit, form }) => (
+        render={({ handleSubmit }) => (
           <form
             onSubmit={(event) => {
               handleSubmit(event);
@@ -56,10 +42,9 @@ const NewComment: FC<NewCommentProps> = ({ cardId }) => {
               name="newComment"
               render={({ input, rest }) => {
                 return (
-                  <Textarea
+                  <TextInput
                     {...input}
                     {...rest}
-                    rows={3}
                     placeholder="Type your comment here"
                     spellCheck={false}
                   />
